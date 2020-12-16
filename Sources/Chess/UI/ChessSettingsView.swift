9cef4babbce11eae52ed3b2a82d6f718b605d2ae
Doug Pedley <dpedley@gmail.com>
2020-12-16 12:52:24 -0800

    delay slider

--- Sources/Chess/UI/ChessSettingsView.swift
+++ Sources/Chess/UI/ChessSettingsView.swift
@@ -24,7 +24,7 @@ public struct ChessSettingsButton: View {
 
 public struct ChessSettingsView: View {
     @EnvironmentObject public var store: ChessStore
-    @State var playingAs: PlayAsButton.Choice = .white
+    @State var robotDelay: TimeInterval = 1
     public var body: some View {
         Form {
             Section(header: Text("White")) {
@@ -38,6 +38,11 @@ public struct ChessSettingsView: View {
                 ChessOpponentSelector(player: Chess.Robot(side: .black))
                 ChessOpponentSelector(player: Chess.Robot.GreedyBot(side: .black))
                 ChessOpponentSelector(player: Chess.Robot.CautiousBot(side: .black))
+            }
+            Section(header: Text("Robot Move Delay \(robotDelay) seconds")) {
+                Slider(value: $robotDelay, in: 0.1...10, onEditingChanged: { _ in
+                    store.game.setRobotPlaybackSpeed(robotDelay)
+                })
             }
             Section(header: Text("Colors")) {
                 BoardColorSelector(.brown)
@@ -54,40 +59,6 @@ public struct ChessSettingsView: View {
                 })
             }
         }.foregroundColor(.black)
-    }
-    func playerSelectedWhite() {
-        var robot = Chess.Robot(side: .black)
-        if let black = store.game.black as? Chess.Robot {
-            robot = black
-        } else if let white = store.game.white as? Chess.Robot {
-            robot = white
-            robot.side = .black
-        }
-        store.game.white = Chess.HumanPlayer(side: .white)
-        store.game.black = robot
-    }
-    func playerSelectedBlack() {
-        var robot = Chess.Robot(side: .white)
-        if let white = store.game.white as? Chess.Robot {
-            robot = white
-        } else if let black = store.game.black as? Chess.Robot {
-            robot = black
-            robot.side = .white
-        }
-        store.game.black = Chess.HumanPlayer(side: .black)
-        store.game.white = robot
-    }
-    func playerSelectedWatcher() {
-        if !store.game.white.isBot() {
-            let robot: Chess.Robot = store.game.black as? Chess.Robot ?? Chess.Robot(side: .white)
-            robot.side = .white
-            store.game.white = robot
-        }
-        if !store.game.black.isBot() {
-            let robot: Chess.Robot = store.game.white as? Chess.Robot ?? Chess.Robot(side: .black)
-            robot.side = .black
-            store.game.black = robot
-        }
     }
     public init() {}
 }
