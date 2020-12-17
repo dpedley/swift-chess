f22052e0e7aff293bc45e6131415e97632a057d0
Doug Pedley <dpedley@gmail.com>
2020-12-16 20:46:04 -0800

    Made the square view selection types into views

--- Sources/Chess/UI/BoardView.swift
+++ Sources/Chess/UI/BoardView.swift
@@ -8,6 +8,85 @@
 import SwiftUI
 import Combine
 
+public struct SquareBackground: View {
+    @EnvironmentObject public var store: ChessStore
+    let position: Chess.Position
+    public var body: some View {
+        Rectangle() // The square background
+            .fill(color(store.environment.theme.color, for: position))
+            .aspectRatio(1, contentMode: .fill)
+    }
+    public func color(_ themeColor: Chess.UI.BoardColor, for position: Chess.Position) -> Color {
+        let evenSquare: Bool = (position.rank + position.fileNumber) % 2 == 0
+        return evenSquare ? themeColor.dark : themeColor.light
+    }
+    public init(_ idx: Int) {
+        self.position = Chess.Position(idx)
+    }
+}
+
+public struct SquareMoveHighlight: View {
+    @EnvironmentObject public var store: ChessStore
+    let position: Chess.Position
+    public var body: some View {
+        guard store.environment.preferences.highlightLastMove,
+              let move = store.game.board.lastMove else {
+            return AnyView(EmptyView())
+        }
+        guard move.start == position
+                || move.end == position else {
+            return AnyView(EmptyView())
+        }
+        let highlight = Rectangle()
+            .fill(Self.chessMoveHighlight)
+            .aspectRatio(1, contentMode: .fill)
+        return AnyView(highlight)
+    }
+    static let chessMoveHighlight = Color(.sRGB, red: 0.0, green: 0.1, blue: 0.9, opacity: 0.3)
+    public init(_ idx: Int) {
+        self.position = Chess.Position(idx)
+    }
+}
+
+public struct SquareSelected: View {
+    @EnvironmentObject public var store: ChessStore
+    let position: Chess.Position
+    public var body: some View {
+        guard store.environment.preferences.highlightLastMove,
+              store.game.board.squares[position].selected else {
+            return AnyView(EmptyView())
+        }
+        let selected = Rectangle()
+            .fill(SquareMoveHighlight.chessMoveHighlight)
+            .aspectRatio(1, contentMode: .fill)
+        return AnyView(selected)
+    }
+    public init(_ idx: Int) {
+        self.position = Chess.Position(idx)
+    }
+}
+
+public struct SquareTargeted: View {
+    @EnvironmentObject public var store: ChessStore
+    let position: Chess.Position
+    public var body: some View {
+        guard store.environment.preferences.highlightChoices,
+              store.game.board.squares[position].targetedBySelected else {
+            return AnyView(EmptyView())
+        }
+        let selected = GeometryReader { geometry in
+            Circle()
+                .inset(by: geometry.size.width / 3)
+                .fill(Self.chessChoiceHighlight)
+        }
+        return AnyView(selected)
+    }
+    static let chessChoiceHighlight = Color(.sRGB, red: 1.0, green: 0.0, blue: 0.0, opacity: 0.3)
+    public init(_ idx: Int) {
+        self.position = Chess.Position(idx)
+    }
+}
+
 public struct BoardView: View {
     @EnvironmentObject public var store: ChessStore
     public let columns: [GridItem] = .init(repeating: .chessFile, count: 8)
@@ -17,18 +96,10 @@ public struct BoardView: View {
                 LazyVGrid(columns: columns, spacing: 0) {
                     ForEach(0..<64) { idx in
                         ZStack {
-                            Rectangle() // The square background
-                                .fill(color(store.environment.theme.color, for: idx))
-                                .aspectRatio(1, contentMode: .fill)
-                            Rectangle()
-                                .fill(highlight(store.environment.preferences.highlightLastMove,
-                                                lastMove: store.game.board.lastMove,
-                                                for: idx))
-                                .aspectRatio(1, contentMode: .fill)
-                            Circle()
-                                .inset(by: geometry.size.width / 3)
-                                .fill(targeted(store.environment.preferences.highlightChoices,
-                                               square: store.game.board.squares[idx]))
+                            SquareBackground(idx)
+                            SquareMoveHighlight(idx)
+                            SquareSelected(idx)
+                            SquareTargeted(idx)
                             PieceView(position: idx)
                                 .onDrag({ NSItemProvider(object: Chess.Position(idx).FEN as NSString) })
                         }
@@ -43,28 +114,6 @@ public struct BoardView: View {
                    alignment: .center)
             .drawingGroup()
         }
-    }
-    static let chessMoveHighlight = Color(.sRGB, red: 0.0, green: 0.1, blue: 0.9, opacity: 0.3)
-    static let chessChoiceHighlight = Color(.sRGB, red: 1.0, green: 0.0, blue: 0.0, opacity: 0.3)
-    public func highlight(_ showHighlight: Bool, lastMove: Chess.Move?, for index: Int) -> Color {
-        guard showHighlight, let lastMove = lastMove else {
-            return .clear
-        }
-        guard lastMove.start == index
-                || lastMove.end == index else {
-            return .clear
-        }
-        return BoardView.chessMoveHighlight
-    }
-    public func targeted(_ showChoice: Bool, square: Chess.Square) -> Color {
-        guard showChoice, square.targetedBySelected else {
-            return .clear
-        }
-        return BoardView.chessChoiceHighlight
-    }
-    public func color(_ themeColor: Chess.UI.BoardColor, for index: Int) -> Color {
-        let evenSquare: Bool = (Chess.Position(index).rank + Chess.Position(index).fileNumber) % 2 == 0
-        return evenSquare ? themeColor.dark : themeColor.light
     }
     public init() {}
 }
