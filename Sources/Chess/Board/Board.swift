9c843560cffb0c795216a2acd311322c0e693318
Doug Pedley <dpedley@gmail.com>
2020-12-19 00:43:16 -0800

    public board

--- Sources/Chess/Board/Board.swift
+++ Sources/Chess/Board/Board.swift
@@ -10,10 +10,10 @@ import Foundation
 public extension Chess {
     struct Board {
         let populateExpensiveVisuals: Bool
-        var squares: [Square] = []
-        var turns: [Turn] = []
-        var isInCheck: Bool?
-        var playingSide: Side = .white {
+        public var squares: [Square] = []
+        public var turns: [Turn] = []
+        public var isInCheck: Bool?
+        public var playingSide: Side = .white {
             didSet {
                 // Only do this for an active visual board, it's expensive for a NilVisualizer
                 if self.populateExpensiveVisuals {
@@ -23,24 +23,24 @@ public extension Chess {
                 }
             }
         }
-        var lastMove: Move? {
+        public var lastMove: Move? {
             guard let lastTurn = turns.last else { return nil }
             if let move = lastTurn.black {
                 return move
             }
             return lastTurn.white
         }
-        var fullMoves = 1 // This is intentionally 1 even at the games start.
+        public var fullMoves = 1 // This is intentionally 1 even at the games start.
         var enPassantPosition: Position? { return lastEnPassantPosition() }
-        var squareForActiveKing: Chess.Square { return findKing(playingSide) }
-        init(populateExpensiveVisuals: Bool = false) {
+        public var squareForActiveKing: Chess.Square { return findKing(playingSide) }
+        public init(populateExpensiveVisuals: Bool = false) {
             self.populateExpensiveVisuals = populateExpensiveVisuals
             for index in 0...63 {
                 let newSquare = Square(position: Position(index))
                 squares.append(newSquare)
             }
         }
-        init(FEN: String, populateExpensiveVisuals: Bool = false) {
+        public init(FEN: String, populateExpensiveVisuals: Bool = false) {
             self.init(populateExpensiveVisuals: populateExpensiveVisuals)
             self.resetBoard(FEN: FEN)
         }
