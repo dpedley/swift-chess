bfe439e35bdd2d84430b432d7a07f3012af1398d
Doug Pedley <dpedley@gmail.com>
2020-12-21 14:29:37 -0800

    clean up

--- Sources/Chess/Board/BoardVariant.swift
+++ Sources/Chess/Board/BoardVariant.swift
@@ -4,7 +4,6 @@
 //  Created by Douglas Pedley on 1/9/19.
 //
 import Foundation
-import GameplayKit
 
 public extension Chess {
     enum BoardVariantError: Error {
@@ -12,9 +11,9 @@ public extension Chess {
         case canOnlyUndoLastMove
     }
     class BoardVariant: NSObject {
-        private(set) public var originalFEN: String
-        private(set) public var changes: [Chess.Move] = []
-        private(set) public var board: Board
+        internal(set) public var originalFEN: String
+        internal(set) public var changes: [Chess.Move] = []
+        internal(set) public var board: Board
         public func pieceWeights() -> GameAnalysis {
             return board.pieceWeights()
         }
@@ -53,119 +52,5 @@ public extension Chess {
             self.board = originalBoard
             self.originalFEN = originalBoard.FEN
         }
-    }
-}
-
-public extension Chess {
-    class GameModelPlayer: NSObject, GKGameModelPlayer {
-        public let playerId: Int
-        public let side: Chess.Side
-        public init(_ playerId: Int, side: Chess.Side) {
-            self.playerId = playerId
-            self.side = side
-        }
-    }
-    class GameModelUpdate: NSObject, GKGameModelUpdate {
-        let variant: Chess.BoardVariant
-        public var value: Int = 0
-        public required init(_ variant: Chess.BoardVariant) {
-            guard let move = variant.changes.last else {
-                fatalError("Game model update assume at least one move.")
-            }
-            self.variant = variant
-            guard let rollback = variant.copy() as? Chess.BoardVariant else { return }
-            do {
-                try rollback.undoMove(move, deepVariant: true)
-                self.value = Self.evaluate(before: rollback, after: variant, side: move.side)
-            } catch let error {
-                Chess.log.error("GameModelUpdate: error rollback - \(error.localizedDescription)")
-            }
-        }
-        static func evaluate(before: Chess.BoardVariant, after: Chess.BoardVariant, side: Chess.Side) -> Int {
-            return Int(1000 * (after.pieceWeights().value(for: side) - before.pieceWeights().value(for: side)))
-        }
-    }
-}
-
-extension Chess.BoardVariant: GKGameModel {
-    static let black = Chess.GameModelPlayer(0, side: .black)
-    static let white = Chess.GameModelPlayer(1, side: .white)
-    public var players: [GKGameModelPlayer]? {
-        return [Self.black, Self.white]
-    }
-    public var activePlayer: GKGameModelPlayer? {
-        board.playingSide == .black ? Self.black : Self.white
-    }
-    public func setGameModel(_ gameModel: GKGameModel) {
-        guard let model = gameModel as? Chess.BoardVariant else {
-            Chess.log.error("Couldn't use the gameModel \(gameModel)")
-            return
-        }
-        self.originalFEN = model.originalFEN
-        self.board = model.board
-        self.changes = model.changes
-    }
-    public func gameModelUpdates(for player: GKGameModelPlayer) -> [GKGameModelUpdate]? {
-        guard let player = player as? Chess.GameModelPlayer else { return nil }
-        let variants = board.createValidVariants(for: player.side)
-        return variants?.compactMap {
-            guard $0.move != nil else { return nil }
-            return Chess.GameModelUpdate($0)
-        }
-    }
-    public func apply(_ gameModelUpdate: GKGameModelUpdate) {
-        guard let update = gameModelUpdate as? Chess.GameModelUpdate,
-              let move = update.variant.changes.last else {
-            Chess.log.error("Couldn't apply GameplayKit update: \(gameModelUpdate)")
-            return
-        }
-        do {
-            try makeMove(move, deepVariant: true)
-        } catch let error {
-            Chess.log.error("GameModel apply move failed: \(error.localizedDescription)")
-        }
-    }
-    public func unapplyGameModelUpdate(_ gameModelUpdate: GKGameModelUpdate) {
-        guard let update = gameModelUpdate as? Chess.GameModelUpdate,
-              let move = update.variant.changes.last else {
-            Chess.log.error("Couldn't apply GameplayKit update: \(gameModelUpdate)")
-            return
-        }
-        do {
-            try undoMove(move, deepVariant: true)
-        } catch let error {
-            Chess.log.error("GameModel undo move failed: \(error.localizedDescription)")
-        }
-    }
-    public func score(for player: GKGameModelPlayer) -> Int {
-        guard let player = player as? Chess.GameModelPlayer else {
-            fatalError("Misconfigured gameplaykit.")
-        }
-        let score = Int(board.pieceWeights().value(for: player.side) * 1000)
-        return score
-    }
-    public func isWin(for player: GKGameModelPlayer) -> Bool {
-        guard let player = player as? Chess.GameModelPlayer else {
-            fatalError("Misconfigured gameplaykit.")
-        }
-        guard player.side == board.playingSide.opposingSide, board.isKingMated() else {
-            return false
-        }
-        return true
-    }
-    public func isLoss(for player: GKGameModelPlayer) -> Bool {
-        guard let player = player as? Chess.GameModelPlayer else {
-            fatalError("Misconfigured gameplaykit.")
-        }
-        guard player.side == board.playingSide, board.isKingMated() else {
-            return false
-        }
-        return true
-    }
-    public func copy(with zone: NSZone? = nil) -> Any {
-        let copyBoard = Chess.BoardVariant(originalFEN: originalFEN)
-        copyBoard.board = board
-        copyBoard.changes = changes
-        return copyBoard
     }
 }
