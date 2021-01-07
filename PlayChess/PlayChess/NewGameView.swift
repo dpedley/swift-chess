4075cf9f7f45504b47595e7f66227e8fefe93491
Doug Pedley <dpedley@gmail.com>
2021-01-06 19:52:48 -0800

    PR readiness

--- PlayChess/PlayChess/NewGameView.swift
+++ PlayChess/PlayChess/NewGameView.swift
@@ -10,9 +10,21 @@ import Chess
 import SwiftUI
 
 enum RobotStories {
-    static let randomBot = "The easiest challenger in your robot training group. This robot will move without thinking. It will pick any move that is allowed, even if it is a blunder. Try this robot out if you're still learning the game"
-    static let greedyBot = "Attack and capture is the only thing this bot thinks about. Once you've learned how to move the pieces, you'll find it becomes easy to out think this robot. Make sure to protect your pieces, and GreedyBot will capture and blunder."
-    static let cautiousBot = "The last of the learning bots. CautiousBot focuses on avoiding risky moves, and trying to trade when it's favorable. When you can beat CautiousBot, you can feel comfortable that you get the basics of Chess."
+    static let randomBot = """
+    The easiest challenger in your robot training group. This robot will move without
+     thinking. It will pick any move that is allowed, even if it is a blunder. Try this
+     robot out if you're still learning the game
+    """.replacingOccurrences(of: "\n", with: "")
+    static let greedyBot = """
+    Attack and capture is the only thing this bot thinks about. Once you've learned how to
+     move the pieces, you'll find it becomes easy to out think this robot. Make sure to
+     protect your pieces, and GreedyBot will capture and blunder.
+    """.replacingOccurrences(of: "\n", with: "")
+    static let cautiousBot = """
+    The last of the learning bots. CautiousBot focuses on avoiding risky moves, and trying
+     to trade when it's favorable. When you can beat CautiousBot, you can feel comfortable
+     that you get the basics of Chess.
+    """.replacingOccurrences(of: "\n", with: "")
 }
 
 struct RobotPlacard: View {
@@ -53,7 +65,7 @@ struct RobotPlacard: View {
 struct NewGameView: View {
     @EnvironmentObject var store: ChessStore
     var startMessage: String {
-        guard let _ = store.game.white as? Chess.HumanPlayer else {
+        guard store.game.white is Chess.HumanPlayer else {
             return "Start the game with the play icon"
         }
         return "Make your first more to start the game"
